8af5e32ece19fd7fed68324a6e63ee6b6d974cb8
Gavin COMPTON <Gavin.COMPTON@EDUCATION.GOV.UK>
2023-12-20 15:12:27 +0000

    Initial creation of NuGet package.

---
+++ DFC.Common.SharedContent.Pkg.Netcore.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace DFC.Common.SharedContent.Pkg.Netcore.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ DFC.Common.SharedContent.Pkg.Netcore/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace DFC.Common.SharedContent.Pkg.Netcore;
+
+public class Class1
+{
+
+}
