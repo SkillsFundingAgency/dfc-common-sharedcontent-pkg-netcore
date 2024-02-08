1dc667b86538ff2b74093ce51ee62ccc25b8ad9b
hnandra <32364605+hnandra@users.noreply.github.com>
2024-02-08 17:55:01 +0000

    Added JSON serialization files

---
+++ DFC.Common.SharedContent.Pkg.Netcore/Model/ContentItems/Dysac/JobProfileOverview.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems.Dysac
+{
+    public class JobProfileOverview
+    {
+        [JsonProperty("overview")]
+        public string Overview { get; set; }
+
+        [JsonProperty("contentItemId")]
+        public string ContentItemId { get; set; }
+    }
+}
