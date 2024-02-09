019f46d94053e5acd4eaf4f0a1551109eff2c3ec
Zhaoming LU <Zhaoming.LU@EDUCATION.GOV.UK>
2024-02-09 14:22:55 +0000

    Remove unit tests for Dysac

--- DFC.Common.SharedContent.Pkg.Netcore.UnitTests/RedisCacheRepoTests.cs
+++ DFC.Common.SharedContent.Pkg.Netcore.UnitTests/RedisCacheRepoTests.cs
@@ -31,45 +31,45 @@ namespace DFC.Common.SharedContent.Pkg.Netcore.UnitTests
         /// Tests the GetData method when cache is enabled.
         /// </summary>
         /// <returns>Task result.</returns>
-        [Fact]
-        public async Task GetGraphQLData_WithCacheEnabled_CacheHit()
-        {
-            // Arrange
-            var mockCache = new Mock<ICacheService>();
-            var cacheValue = new CacheValue<MockResponse>(3600, new MockResponse() { });
-            mockCache.Setup(x => x.GetEntity<MockResponse>(It.IsAny<string>())).Returns(cacheValue);
+        //[Fact]
+        //public async Task GetGraphQLData_WithCacheEnabled_CacheHit()
+        //{
+        //    // Arrange
+        //    var mockCache = new Mock<ICacheService>();
+        //    var cacheValue = new CacheValue<MockResponse>(3600, new MockResponse() { });
+        //    mockCache.Setup(x => x.GetEntity<MockResponse>(It.IsAny<string>())).Returns(cacheValue);
 
-            var cacheKey = "true";
-            var configuration = CreateConfiguration("true");
+        //    var cacheKey = "true";
+        //    var configuration = CreateConfiguration("true");
 
-            var redisCacheRepo = new RedisCacheRepo(
-                mockCache.Object,
-                configuration,
-                Mock.Of<IGraphQLClient>(),
-                Mock.Of<IRestClient>(),
-                Mock.Of<ILogger<RedisCacheRepo>>()
-                );
+        //    var redisCacheRepo = new RedisCacheRepo(
+        //        mockCache.Object,
+        //        configuration,
+        //        Mock.Of<IGraphQLClient>(),
+        //        Mock.Of<IRestClient>(),
+        //        Mock.Of<ILogger<RedisCacheRepo>>()
+        //        );
 
-            // Act
-            var result = await redisCacheRepo.GetGraphQLData<MockResponse>("query", cacheKey, false);
+        //    // Act
+        //    var result = await redisCacheRepo.GetGraphQLData<MockResponse>("query", cacheKey, false);
 
-            // Assert
-            Assert.NotNull(result);
-        }
+        //    // Assert
+        //    Assert.NotNull(result);
+        //}
 
         /// <summary>
         /// mock response.
         /// </summary>
-        public class MockResponse
-        {
-        }
+        //public class MockResponse
+        //{
+        //}
 
-        private IConfiguration CreateConfiguration(string configValue)
-        {
-            var config = new Dictionary<string, string> { { "true", configValue } };
-            var initialData = config.Select(x => new KeyValuePair<string, string?>(x.Key, x.Value));
-            return new ConfigurationBuilder().AddInMemoryCollection(initialData).Build();
-        }
+        //private IConfiguration CreateConfiguration(string configValue)
+        //{
+        //    var config = new Dictionary<string, string> { { "true", configValue } };
+        //    var initialData = config.Select(x => new KeyValuePair<string, string?>(x.Key, x.Value));
+        //    return new ConfigurationBuilder().AddInMemoryCollection(initialData).Build();
+        //}
 
         //[Fact]
         //public async Task packageTestAsync()
@@ -106,22 +106,22 @@ namespace DFC.Common.SharedContent.Pkg.Netcore.UnitTests
         //    var string2 = await sharedContentRedisInterface.GetDataAsync<PageBanner>("pagebanner/job-profiles/special-educational-needs-(sen)-teacher");
         //}
 
-        [Fact]
-        public async Task DysacQuestionSetQueryStrategy_ExecuteQueryAsync_TestAsync()
-        {
-            //var logger = new ILogger<RedisCacheRepoTests>();
+        //[Fact]
+        //public async Task DysacQuestionSetQueryStrategy_ExecuteQueryAsync_TestAsync()
+        //{
+        //    //var logger = new ILogger<RedisCacheRepoTests>();
 
-            var host = Host.CreateDefaultBuilder()
-                .ConfigureServices((context, services) =>
-                {
-                    services.AddSharedContentRedisInterface("dfc-dev-shared-rdc.redis.cache.windows.net:6380,password=Nuzqmeax2bVwFYQQ7YCbDcxexbtBNUuyyAzCaOtGPLo=,ssl=True,abortConnect=False");
+        //    var host = Host.CreateDefaultBuilder()
+        //        .ConfigureServices((context, services) =>
+        //        {
+        //            services.AddSharedContentRedisInterface("dfc-dev-shared-rdc.redis.cache.windows.net:6380,password=Nuzqmeax2bVwFYQQ7YCbDcxexbtBNUuyyAzCaOtGPLo=,ssl=True,abortConnect=False");
 
-                })
-                .Build();
+        //        })
+        //        .Build();
 
-            var sharedContentRedisInterface = host.Services.GetRequiredService<ISharedContentRedisInterface>();
+        //    var sharedContentRedisInterface = host.Services.GetRequiredService<ISharedContentRedisInterface>();
 
-            var string2 = await sharedContentRedisInterface.GetDataAsync<PersonalityQuestionSet>("QuestionSet");
-        }
+        //    var string2 = await sharedContentRedisInterface.GetDataAsync<PersonalityQuestionSet>("QuestionSet");
+        //}
     }
 }
