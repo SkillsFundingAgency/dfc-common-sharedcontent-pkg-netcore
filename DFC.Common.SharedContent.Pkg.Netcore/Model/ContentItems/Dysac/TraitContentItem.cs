d1cc9acc08d1cfaa7547a6b2098e2aaa7e6fd7ee
Zhaoming LU <Zhaoming.LU@EDUCATION.GOV.UK>
2024-02-16 14:51:19 +0000

    Add nodeId for question set

--- DFC.Common.SharedContent.Pkg.Netcore/Model/ContentItems/Dysac/TraitContentItem.cs
+++ DFC.Common.SharedContent.Pkg.Netcore/Model/ContentItems/Dysac/TraitContentItem.cs
@@ -1,8 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿using DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems.PageBanner;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems.Dysac
@@ -11,6 +13,9 @@ namespace DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems.Dysac
     {
         [JsonProperty("displayText")]
         public string DisplayText { get; set; }
+
+        [JsonPropertyName("graphSync")]
+        public GraphSync GraphSync { get; set; }
 
         [JsonProperty("jobProfileCategories")]
         public JobProfileCategories JobProfileCategories { get; set; }
