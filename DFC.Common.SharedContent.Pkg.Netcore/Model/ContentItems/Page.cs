d324a55d51c3d6fbde7a5ccfe8da6ab1ff3946a3
James <152189499+JamesPrior-07@users.noreply.github.com>
2024-02-20 16:35:51 +0000

    Make Page Response items nullable

--- DFC.Common.SharedContent.Pkg.Netcore/Model/ContentItems/Page.cs
+++ DFC.Common.SharedContent.Pkg.Netcore/Model/ContentItems/Page.cs
@@ -5,40 +5,40 @@ namespace DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems;
 public class Page
 {
     [JsonPropertyName("displayText")]
-    public string DisplayText { get; set; }
+    public string? DisplayText { get; set; }
 
     [JsonPropertyName("description")]
-    public object Description { get; set; }
+    public object? Description { get; set; }
 
     [JsonPropertyName("pageLocation")]
-    public PageLocation PageLocation { get; set; }
+    public PageLocation? PageLocation { get; set; }
 
     [JsonPropertyName("breadcrumb")]
-    public Breadcrumb Breadcrumb { get; set; }
+    public Breadcrumb? Breadcrumb { get; set; }
 
     [JsonPropertyName("useBrowserWidth")]
     public bool? UseBrowserWidth { get; set; }
 
     [JsonPropertyName("showBreadcrumb")]
-    public bool ShowBreadcrumb { get; set; }
+    public bool? ShowBreadcrumb { get; set; }
 
     [JsonPropertyName("showHeroBanner")]
-    public bool ShowHeroBanner { get; set; }
+    public bool? ShowHeroBanner { get; set; }
 
     [JsonPropertyName("herobanner")]
-    public Herobanner Herobanner { get; set; }
+    public Herobanner? Herobanner { get; set; }
 
     [JsonPropertyName("useInTriageTool")]
-    public bool UseInTriageTool { get; set; }
+    public bool? UseInTriageTool { get; set; }
 
     [JsonPropertyName("triageToolSummary")]
-    public TriageToolSummary TriageToolSummary { get; set; }
+    public TriageToolSummary? TriageToolSummary { get; set; }
 
     [JsonPropertyName("triageToolFilters")]
-    public TriageToolFilters TriageToolFilters { get; set; }
+    public TriageToolFilters? TriageToolFilters { get; set; }
 
     [JsonPropertyName("flow")]
-    public Flow Flow { get; set; }
+    public Flow? Flow { get; set; }
 
     [JsonPropertyName("form")]
     public Form? Form { get; set; }
