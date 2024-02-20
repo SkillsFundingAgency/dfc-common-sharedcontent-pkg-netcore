c965c55e98cdd63f6f4232734f47239df697ed53
amjones99 <59894100+amjones99@users.noreply.github.com>
2024-02-20 14:00:29 +0000

    Readd pageurls changes

--- DFC.Common.SharedContent.Pkg.Netcore/Model/ContentItems/ContentItem.cs
+++ DFC.Common.SharedContent.Pkg.Netcore/Model/ContentItems/ContentItem.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems.PageBanner;
+using Newtonsoft.Json;
 
 namespace DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems
 {
@@ -6,5 +7,17 @@ namespace DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems
     {
         [JsonProperty("displayText")]
         public string DisplayText { get; set; }
+
+        [JsonProperty("isGlobal")]
+        public bool IsGlobal { get; set; }
+
+        [JsonProperty("isActive")]
+        public bool IsActive { get; set; }
+
+        [JsonProperty("content")]
+        public Content Content { get; set; }
+
+        [JsonProperty("graphSync")]
+        public GraphSync GraphSync { get; set; }
     }
 }
