c01a5d8eaf2f9a9bf587c65d853edc6d67beb12b
James <152189499+JamesPrior-07@users.noreply.github.com>
2024-03-04 15:48:12 +0000

    Added Page sitemap model/Response retrieval

---
+++ DFC.Common.SharedContent.Pkg.Netcore/Infrastructure/Strategy/SitemapStrategy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DFC.Common.SharedContent.Pkg.Netcore.Interfaces;
+using DFC.Common.SharedContent.Pkg.Netcore.Model.Response;
+using GraphQL.Client.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace DFC.Common.SharedContent.Pkg.Netcore.Infrastructure.Strategy
+{
+    public class SitemapStrategy : ISharedContentRedisInterfaceStrategy<SitemapResponse>
+    {
+        private readonly IGraphQLClient client;
+        private readonly ILogger<SitemapStrategy> logger;
+        private readonly int sitemapLimit = 150;
+
+        public SitemapStrategy(IGraphQLClient client, ILogger<SitemapStrategy> logger)
+        {
+            this.client = client;
+            this.logger = logger;
+        }
+
+        public async Task<SitemapResponse> ExecuteQueryAsync(string key)
+        {
+            string query = $@"query MyQuery {{
+  page(first: {sitemapLimit}) {{
+    sitemap {{
+      changeFrequency
+      exclude
+      overrideSitemapConfig
+      priority
+    }}
+  }}
+}}
+";
+
+            var response = await client.SendQueryAsync<SitemapResponse>(query);
+            return response.Data;
+        }
+    }
+}
---
+++ DFC.Common.SharedContent.Pkg.Netcore/Model/ContentItems/Sitemap/PageSitemapModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems.Sitemap
+{
+    public class PageSitemapModel
+    {
+        [JsonPropertyName("sitemap")]
+        public SitemapModel? Sitemap { get; set; }
+    }
+
+    public partial class SitemapModel
+    {
+        [JsonPropertyName("changeFrequency")]
+        public string? ChangeFrequency { get; set; }
+
+        [JsonPropertyName("exclude")]
+        public bool? Exclude { get; set; }
+
+        [JsonPropertyName("overrideSitemapConfig")]
+        public bool? OverrideSitemapConfig { get; set; }
+
+        [JsonPropertyName("priority")]
+        public double? Priority { get; set; }
+    }
+}
