03515d006a7dbc9dfbe0b932d608802f168e4861
Gavin COMPTON <Gavin.COMPTON@EDUCATION.GOV.UK>
2024-04-24 16:18:57 +0100

    Removed hidden alternative title as this isn't used.

--- DFC.Common.SharedContent.Pkg.Netcore/Model/ContentItems/HiddenAlternativeTitle.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems
-{
-    public class HiddenAlternativeTitle
-    {
-        public List<string>? ContentItems { get; set; }
-    }
-}
