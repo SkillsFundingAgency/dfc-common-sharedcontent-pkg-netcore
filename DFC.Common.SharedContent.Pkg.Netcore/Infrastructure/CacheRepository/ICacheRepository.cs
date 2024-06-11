3c8cd757e03604f95f5f71ee2549f00ee111913f
Gavin COMPTON <Gavin.COMPTON@EDUCATION.GOV.UK>
2024-06-11 08:27:43 +0100

    Interim check in.  WIP

---
+++ DFC.Common.SharedContent.Pkg.Netcore/Infrastructure/CacheRepository/ICacheRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace DFC.Common.SharedContent.Pkg.Netcore.Infrastructure.CacheRepository
+{
+    public interface ICacheRepository
+    {
+        Task<TResponse> GetQueryWithPagination<TResponse, TRecordType>(string query, Func<TResponse, List<TRecordType>> recordSelectorFunc, Func<List<TRecordType>, TResponse> mergerFunc, int paginationCount = 100)
+            where TResponse : class;
+    }
+}
---
+++ DFC.Common.SharedContent.Pkg.Netcore/Infrastructure/Pagination/GraphQLConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace DFC.Common.SharedContent.Pkg.Netcore.Infrastructure.Pagination
+{
+    /// <summary>
+    /// The GraphQL config.
+    /// </summary>
+    public class GraphQLConfig
+    {
+        /// <summary>
+        /// Skip count token in queries.
+        /// </summary>
+        public const string SkipCountToken = "${SKIPCOUNT}";
+
+        /// <summary>
+        /// Pagination count token in queries.
+        /// </summary>
+        public const string PaginationCountToken = "${PAGINATIONCOUNT}";
+    }
+}
