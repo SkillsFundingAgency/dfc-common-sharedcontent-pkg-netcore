f7bab3ab5cdbbc2ad99e2f7eea97b2cd9a6c2657
Siva MURUGANANDHAM <Siva.MURUGANANDHAM@EDUCATION.GOV.UK>
2024-08-15 09:55:21 +0100

    Removed white spaces

--- DFC.Common.SharedContent.Pkg.Netcore/Model/Common/TriageLevelOne.cs
+++ DFC.Common.SharedContent.Pkg.Netcore/Model/Common/TriageLevelOne.cs
@@ -3,7 +3,6 @@ using System.Text.Json.Serialization;
 
 namespace DFC.Common.SharedContent.Pkg.Netcore.Model.Common
 {
-
     public class TriageLevelOne
     {
         [JsonPropertyName("contentItems")]
