25447aa7825b50de8a385db84bb2f6ef294e8ccd
Siva MURUGANANDHAM <Siva.MURUGANANDHAM@EDUCATION.GOV.UK>
2024-09-23 00:04:03 +0100

    updated cache key

--- DFC.Common.SharedContent.Pkg.Netcore/Constant/ApplicationKeys.cs
+++ DFC.Common.SharedContent.Pkg.Netcore/Constant/ApplicationKeys.cs
@@ -37,5 +37,7 @@
         public const string SkillsAll = "Skills/All";
         public const string JobProfileApiSummaryAll = "JobProfileApi/Summary/All";
         public const string TriageToolSpeakToAnAdviser = "SharedContent/5a54b3d6-3001-4c72-a1f9-7e20e11ae30a";
+        public const string TriageToolLookup = "TriageTool/Lookup";
+        public const string TriageResults = "TriageTool/TriageResults";
     }
 }
